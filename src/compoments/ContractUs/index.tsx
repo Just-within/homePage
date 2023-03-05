fcbed462247f9d7d912914232a4e55452731f2f9
peacefulmeditationgroup <peacefulmeditationgroup@gmail.com>
2023-03-05 16:08:53 +0800

    Modify code base

--- src/compoments/ContractUs/index.tsx
+++ src/compoments/ContractUs/index.tsx
@@ -1,58 +1,44 @@
 import styled from 'styled-components';
 
-const Header = styled.div`
-    color: black;
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
 `;
 
-const Label = styled.label`
-    color: #666666;
+const IframeContainer = styled.div`
+    width: 530px;
+    max-width: 530px;
+`;
+
+const StyledIframe = styled.iframe`
+    min-width: 100%;
+    max-width: 100%;
+    height: 833px;
+    border: none;
+    @media screen and (min-width: 500px) {
+        height: 833px;
+    }
 `;
 
 export default function ContractUs() {
     return (
         <section id="contactUs" className="contact-parlex">
-            <div className="parlex-back">
-                <div className="container">
-                    <div className="row">
-                        <Header className="heading text-center">
-                            <h2>Contact Us</h2>
-                            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered.</p>
-                        </Header>
-                    </div>
-                    <div className="row mrgn30">
-                        <form method="post" action="" id="contactfrm" role="form">
-                            <div className="col-sm-12">
-                                <div className="form-group">
-                                    <Label htmlFor="name">Name</Label>
-                                    <input type="text" className="form-control" name="name" id="name" placeholder="Enter name"
-                                        title="Please enter your name (at least 2 characters)" />
-                                </div>
-                                <div className="form-group">
-                                    <Label htmlFor="email">Email</Label>
-                                    <input type="email" className="form-control" name="email" id="email" placeholder="Enter email"
-                                        title="Please enter a valid email address" />
-                                </div>
-                                <div className="form-group">
-                                    <Label htmlFor="comments">Comments</Label>
-                                    <textarea
-                                        name="comment"
-                                        className="form-control"
-                                        id="comments"
-                                        style={{ marginBottom: 15 }}
-                                        cols={3}
-                                        rows={5}
-                                        placeholder="Enter your message…"
-                                        title="Please enter your message (at least 10 characters)"
-                                    >
-                                    </textarea>
-                                    <button name="submit" type="submit" className="btn btn-lg btn-primary" id="submit">Submit</button>
-                                </div>
-                                <div className="result"></div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
+            <Container className="container">
+                <IframeContainer>
+                    <StyledIframe
+                        id="JotFormIFrame-230631047148046"
+                        title="Contract Us" onLoad={() => {
+                            window.parent.scrollTo(0, 0)
+                        }}
+                        allowTransparency
+                        allowFullScreen
+                        allow="geolocation; microphone; camera"
+                        src="https://form.jotform.com/230631047148046"
+                        frameBorder="0"
+                        scrolling="no"
+                    />
+                </IframeContainer>
+            </Container>
         </section>
     );
 }

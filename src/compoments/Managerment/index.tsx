3cfe60cd59d23d8d0d7641bd2bb7699372f7233c
peacefulmeditationgroup <peacefulmeditationgroup@gmail.com>
2023-03-28 21:53:18 +0800

    Modify code base

--- src/compoments/Managerment/index.tsx
+++ src/compoments/Managerment/index.tsx
@@ -1,10 +1,17 @@
+import styled from 'styled-components';
+import { Button } from '../common.style';
+
+const ButtonContainer = styled.div`
+    text-align: center;
+`;
+
 export default function Managerment() {
     return (
         <section id="team" className="page-section">
             <div className="container">
                 <div className="heading text-center">
-                    <h2>Members</h2>
-                    <p>At variations of passages of Lorem Ipsum available, but the majority have suffered alteration..</p>
+                    <h2>Contacts</h2>
+                    <p>Member of our group.</p>
                 </div>
                 <div className="team-content">
                     <div className="row">
@@ -13,10 +20,14 @@ export default function Managerment() {
                                 <div className="member-img">
                                     <img className="img-responsive" src="img/Maggie.png" alt="" />
                                 </div>
-                                <div className="team-title">
+                                {/* <div className="team-title"> */}
+                                <ButtonContainer>
                                     <h4>Maggie</h4>
-                                    {/* <span className="pos">DEAN</span> */}
-                                </div>
+                                    <Button className="btn btn-apply" href="mailto:abc@example.com">Email</Button>
+                                </ButtonContainer>
+
+                                {/* <span className="pos">DEAN</span> */}
+                                {/* </div> */}
                                 {/* <div className="team-socials"> <a href="#"><i className="fa fa-facebook"></i></a> <a href="#"><i
                                     className="fa fa-google-plus"></i></a> <a href="#"><i className="fa fa-twitter"></i></a> <a href="#"><i
                                         className="fa fa-dribbble"></i></a> <a href="#"><i className="fa fa-github"></i></a> </div> */}
@@ -25,9 +36,13 @@ export default function Managerment() {
                         <div className="col-md-3 col-sm-6 col-xs-12">
                             <div className="team-member pDark">
                                 <div className="member-img">
-                                    <img className="img-responsive" src="img/photo-2.jpg" alt="" />
+                                    <img className="img-responsive" src="img/Pegasus.jpg" alt="" />
                                 </div>
-                                <h4>Pegagus</h4>
+                                <ButtonContainer>
+                                    <h4>Pegasus Chen</h4>
+                                    <Button className="btn btn-apply" href="mailto:chanandpega@gmail.com">Email</Button>
+                                </ButtonContainer>
+
                                 {/* <span className="pos">Director</span> */}
                                 {/* <div className="team-socials"> <a href="#"><i className="fa fa-facebook"></i></a> <a href="#"><i
                                     className="fa fa-google-plus"></i></a> <a href="#"><i className="fa fa-twitter"></i></a> <a href="#"><i
@@ -39,7 +54,11 @@ export default function Managerment() {
                                 <div className="member-img">
                                     <img className="img-responsive" src="img/Odina.jpg" alt="" />
                                 </div>
-                                <h4>Odina</h4>
+                                <ButtonContainer>
+                                    <h4>Odina Wang</h4>
+                                    <Button className="btn btn-apply" href="mailto:seawang060@gmail.com">Email</Button>
+                                </ButtonContainer>
+
                                 {/* <span className="pos">HOD</span> */}
                                 {/* <div className="team-socials"> <a href="#"><i className="fa fa-facebook"></i></a> <a href="#"><i
                                     className="fa fa-google-plus"></i></a> <a href="#"><i className="fa fa-twitter"></i></a> <a href="#"><i
@@ -50,8 +69,13 @@ export default function Managerment() {
                             <div className="team-member pDark">
                                 <div className="member-img">
                                     <img className="img-responsive" src="img/Winnie.png" alt="" />
+
                                 </div>
-                                <h4>Winnie</h4>
+                                <ButtonContainer>
+                                    <h4>Winnie</h4>
+                                    <Button className="btn btn-apply" href="mailto:chanandpega@gmail.com">Email</Button>
+                                </ButtonContainer>
+
                                 {/* <span className="pos">Finance</span> */}
                                 {/* <div className="team-socials"> <a href="#"><i className="fa fa-facebook"></i></a> <a href="#"><i
                                     className="fa fa-google-plus"></i></a> <a href="#"><i className="fa fa-twitter"></i></a> <a href="#"><i

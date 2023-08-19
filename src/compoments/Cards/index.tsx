0e1048420785817cb7fcadfda30caa1f415b78df
Jeff_Hsieh <mediocre0528@gmail.com>
2023-08-20 06:55:28 +0800

    Modify code base

--- src/compoments/Cards/index.tsx
+++ src/compoments/Cards/index.tsx
@@ -19,66 +19,22 @@ const CardContainer = styled.div`
     justify-content: center;
 `;
 
-// const StyledCard = styled(Card)`
-//     margin-bottom: 20px;
-//     width: 240px;
-//     border-radius: 0;
-//     &:before {
-//         content: "";
-//         width: 60px;
-//         height: 60px;
-//         background: yellow;
-//         float: right;
-//         position: absolute;
-//         z-index: -1;
-//         top: -5px;
-//         right: -5px;
-//     }
-//     &:after {
-//         content: "";
-//         width: 60px;
-//         height: 60px;
-//         background: yellow;
-//         float: right;
-//         position: absolute;
-//         z-index: -1;
-//         bottom: -5px;
-//         left: -5px;
-//     }
-// `;
-
-// const StyledCardMeta = styled(Card.Meta)`
-//     text-align: center;
-// `;
-
 export default function Cards() {
     return (
         <OuterContainer id="content">
             <Container className="container">
                 <div className='row'>
                     <CardContainer className='col-xs-12 col-sm-6 col-lg-3'>
-                        <div
-                        >
-                            {/* <div title="Courses" description="Nullam ac rhoncus sapien, non gravida purus. Alinon elit imperdiet congue. Integer elit imperdiet congue." /> */}
-                        </div>
+                        <div></div>
                     </CardContainer>
                     <CardContainer className='col-xs-12 col-sm-6 col-lg-3'>
-                        <div
-                        >
-                            {/* <div title="Knowledge" description="Nullam ac rhoncus sapien, non gravida purus. Alinon elit imperdiet congue. Integer elit imperdiet congue." /> */}
-                        </div>
+                        <div></div>
                     </CardContainer>
                     <CardContainer className='col-xs-12 col-sm-6 col-lg-3'>
-                        <div
-                        >
-                            {/* <div title="Events" description="Nullam ac rhoncus sapien, non gravida purus. Alinon elit imperdiet congue. Integer elit imperdiet congue." /> */}
-                        </div>
+                        <div></div>
                     </CardContainer>
                     <CardContainer className='col-xs-12 col-sm-6 col-lg-3'>
-                        <div
-                        >
-                            {/* <StyledCardMeta title="Latest News" description="Nullam ac rhoncus sapien, non gravida purus. Alinon elit imperdiet congue. Integer elit imperdiet congue." /> */}
-                        </div>
+                        <div></div>
                     </CardContainer>
                 </div>
             </Container>
--- src/compoments/Footer/index.tsx
+++ src/compoments/Footer/index.tsx
@@ -1,71 +1,15 @@
 export default function Footer() {
     return (
         <>
-            {/* <footer>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-3">
-                            <div className="col">
-                                <h4>Contact us</h4>
-                                <ul>
-                                    <li>1th Avenue, New York - United States</li>
-                                    <li>Phone: +22 342 2345 345 | Fax: +22 724 2342 343 </li>
-                                    <li>Email: <a href="mailto:info@example.com" title="Email Us">info@example.com</a></li>
-                                    <li>Skype: <a href="skype:my.test?call" title="Skype us">my-company</a></li>
-                                </ul>
-                            </div>
-                        </div>
-
-                        <div className="col-md-3">
-                            <div className="col">
-                                <h4>Mailing list</h4>
-                                <p>Lorem ipsum dolor sit amet, ea eum labitur scsstie percipitoleat.</p>
-                                <form className="form-inline">
-                                    <div className="input-group">
-                                        <input type="text" className="form-control" placeholder="Your email address..." />
-                                        <span className="input-group-btn">
-                                            <button className="btn" type="button">Go!</button>
-                                        </span>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-
-                        <div className="col-md-3">
-                            <div className="col col-social-icons">
-                                <h4>Follow us</h4>
-                                <a href="#"><i className="fa fa-facebook-official"></i></a>
-                                <a href="#"><i className="fa fa-google-plus"></i></a>
-                                <a href="#"><i className="fa fa-youtube-play"></i></a>
-                                <a href="#"><i className="fa fa-flickr"></i></a>
-                                <a href="#"><i className="fa fa-linkedin"></i></a>
-                                <a href="#"><i className="fa fa-twitter"></i></a>
-                                <a href="#"><i className="fa fa-skype"></i></a>
-                                <a href="#"><i className="fa fa-pinterest"></i></a>
-                            </div>
-                        </div>
-
-                        <div className="col-md-3">
-                            <div className="col">
-                                <h4>Latest News</h4>
-                                <p>
-                                    Lorem ipsum dolor labitur scsstie per sit amet, ea eum labitur scsstie percipitoleat.
-                                    <br /><br />
-                                    <a href="#" className="btn">Get Mores!</a>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </footer> */}
             <section className="copyright">
                 <div className="container">
                     <div className="row">
-                        <div className="col-sm-12 text-center" style={{ color: '#281915' }}>Discover your true self within at <span style={{ fontWeight: 900 }}>JustWithin</span> </div>
+                        <div className="col-sm-12 text-center" style={{ color: '#281915' }}>
+                            Discover your true self within at <span style={{ fontWeight: 900 }}>JustWithin</span>
+                        </div>
                     </div>
                 </div>
             </section>
         </>
-
     );
 }
